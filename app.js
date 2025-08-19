75447f4831f60c155d97e0b6e73ec5d2fb18fc86
Ezequiel Soria <100471408+kelo72@users.noreply.github.com>
2025-08-19 18:47:37 -0300

    Creacion de la Funcion agregarAmigo

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = [];
+
+function agregarTexto(elemento, texto){
+    let lista = document.getElementById('listaAmigos');
+
+}
+
+function agregarAmigo(){
+    let amigo = document.getElementById("amigo").value;
+    if (amigo != ""){
+        amigos.push(amigo);
+        console.log(amigos);
+    }else{
+        alert('Debes ingresar un nombre de amigo');
+    }
+}
