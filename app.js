3265441b0d710546b818109a5e82f3ee50c68797
Ezequiel Soria <100471408+kelo72@users.noreply.github.com>
2025-08-20 21:09:51 -0300

    Terminamos la funcion de sorteo y agregamos luna funcion para resetear el sorteo

--- app.js
+++ app.js
@@ -16,6 +16,10 @@ function agregarAmigo(){
         return; //Detenemos la función si el nombre está vacío
     }
 
+    //Vaciar elemento "resultados"
+    let sorteo = document.getElementById("resultado");
+    sorteo.innerHTML = '';
+
     //Actualizamos la visualización de la lista en la página
     actualizarListaAmigos();
 
@@ -23,17 +27,15 @@ function agregarAmigo(){
     document.getElementById("amigo").value = '';
 }
 
-/**
- * Actualiza la lista de amigos en el HTML.
- * Crea un elemento <li> por cada amigo en la lista.
- */
+//Actualiza la lista de amigos en el HTML. Crea un elemento <li> por cada amigo en la lista.
+ 
 function actualizarListaAmigos() {
     let elementoLista = document.getElementById("listaAmigos");
     elementoLista.innerHTML = ''; //Limpia la lista actual para no duplicar nombres
 
     for (let amigo of amigos) {
         let item = document.createElement('li'); //Crea un elemento <li>
-        item.textContent = amigo; //Le asigna el nombre del amigo
+        item.textContent = amigo; 
         elementoLista.appendChild(item); //Añade el <li> a la lista <ul> en el HTML
     }
 }
@@ -41,13 +43,21 @@ function actualizarListaAmigos() {
 //Creamos la funcion sortearAmigo
 function sortearAmigo(){
 
-    //Desordenamos la lista de amigos (Algoritmo de Fisher-Yates)
+    //Elegimos un numero al azar
     let elejido = Math.floor(Math.random() * (amigos.length)); 
         
-        if (elejido <= amigos.length){
-            sorteo= document.getElementById("resultado");
-            sorteo.innerHTML = 'El amigo secreto es ' + amigos[elejido] + '.';
+        if (elejido <= amigos.length){ //comparamos con el Array
+            sorteo= document.getElementById("resultado"); 
+            sorteo.innerHTML = 'El amigo secreto es ' + amigos[elejido] + '.'; //Mostramos el resultado
         }else{
-            alert('Primero debes ingresar los nombres')
+            alert('Primero debes ingresar los nombres') //Sino se coloca nombres se avisa 
         }
+        resetearListaAmigos();
     }
+
+//Funcion resetear listaAmigos para poder reiniciar el sorteo    
+function resetearListaAmigos() {
+    let elementoLista = document.getElementById("listaAmigos");
+    elementoLista.innerHTML = ''; //Vacía la lista en el HTML
+    amigos = []; //Resetea el array de amigos
+}
